958a1ac11cb12afc9b741845d77896b34b2c9f75
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-05 21:23:21 +0900

    03 라우터 설정, reset.css 적용

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React from "react"
+import { HashRouter as Router, Route } from 'react-router-dom'
+import Detail from "../routes/Detail";
+import Home from "../routes/Home";
 
 function App() {
   return (
-    <div>APP</div>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/:id" component={Detail} />
+    </Router>
   );
 }
 
-export default App;
+export default App
