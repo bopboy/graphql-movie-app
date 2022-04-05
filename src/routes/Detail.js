b29cde6ea6bb89870e191add3b9cde2b7cf9d013
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-05 23:21:38 +0900

    06 쿼리 2(get movie)

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,28 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { gql } from 'apollo-boost'
+import { useQuery } from '@apollo/react-hooks'
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      summary
+    }
+  }
+`;
 const Detail = () => {
+    const { id } = useParams()
+    const { loading, data } = useQuery(GET_MOVIE, {
+        variables: {
+            id: parseInt(id)
+        }
+    })
+    console.log(loading, data)
+    if (loading) return "Loading..."
+    if (data && data.movie) return data.movie.title
     return (
         <div>Detail</div>
     )
