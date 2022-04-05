ed2167ece221068ca50700c934a579b6cf50a07f
bopboy <39391891+bopboy@users.noreply.github.com>
2022-04-05 22:06:35 +0900

    05 쿼리 1 (get movies)

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import { gql } from 'apollo-boost'
+import { useQuery } from '@apollo/react-hooks'
+
+const GET_MOVIES = gql`
+    {
+        movies {
+            id
+            medium_cover_image
+        }
+    }
+`
 const Home = () => {
+    const { loading, error, data } = useQuery(GET_MOVIES)
+    if (loading) return "Loading..."
+    if (data && data.movies) return data.movies.map(m => <h1 key={m.id}>{m.id}</h1>)
     return (
         <div>Home</div>
     )
